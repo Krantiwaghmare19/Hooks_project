567c33f4d6d377d1e0a1d6fc11b9e0fbab20d404
kranti waghmare <krantiwaghmare21@navgurukul.org>
2023-10-25 10:39:07 +0530

    create ButtonToggle using useState hook

--- src/component/ButtonToggle.js
+++ src/component/ButtonToggle.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 
-	const ButtonToggle=()=>{
-	const [button,setButton]=useState("ON");
-	
-	const handleClick=()=>{
+const ButtonToggle = () => {
+    const [button, setButton] = useState("ON");
 
-		if (button==="ON"){
-			setButton("OFF")
-		}else{
-			setButton("ON")
-		}
-		
-	}
-	
-	return(
-		<div>
-			<button className="btn" onClick={handleClick}>{button}</button>
-		</div>
-	);
-	};
+    const handleClick = () => {
+
+        if (button === "ON") {
+            setButton("OFF")
+        } else {
+            setButton("ON")
+        }
+
+    }
+
+    return (
+        <div>
+            <button className="btn" onClick={handleClick}>{button}</button>
+        </div>
+    );
+};
 export default ButtonToggle;
