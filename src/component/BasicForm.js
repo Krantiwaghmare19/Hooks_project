e76533bb39118f5ed4d504590b3e76dc17ca9822
kranti waghmare <krantiwaghmare21@navgurukul.org>
2023-10-25 10:36:35 +0530

    create BasicForm using useEffect and useState

--- src/component/BasicForm.js
+++ src/component/BasicForm.js
@@ -1,45 +1,45 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 const BasicForm = () => {
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const [allEntry,setAllEntry] = useState([]);
+    const [allEntry, setAllEntry] = useState([]);
 
     const submitForm = (e) => {
         e.preventDefault();
         if (email && password) {
-            const newEntry= {id: new Date().getTime().toString(), email, password};
+            const newEntry = { id: new Date().getTime().toString(), email, password };
             setAllEntry([...allEntry, newEntry]);
 
             setEmail("");
             setPassword("");
-        }else{
+        } else {
             alert("please fill the data");
-        }    
+        }
     }
 
 
 
-    return(
+    return (
         <>
             <div>
-                <form  action="" onSubmit={submitForm} >
+                <form action="" onSubmit={submitForm} >
                     <div>
                         <label htmlFor="email">Email</label>
-                            <input type="text" name="email" id="email" autoComplete="off"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
+                        <input type="text" name="email" id="email" autoComplete="off"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
 
                     <div>
                         <label htmlFor="password">Password</label>
-                            <input type="password" name="password" id="password" autoComplete="off"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
+                        <input type="password" name="password" id="password" autoComplete="off"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
 
-                            />
+                        />
                     </div>
 
                     <button type="submit">Login</button>
@@ -47,9 +47,9 @@ const BasicForm = () => {
                 </form>
                 <div>
                     {
-                        allEntry.map((currentElement)=>{
-                            const { id, email, password } =currentElement;
-                            return(
+                        allEntry.map((currentElement) => {
+                            const { id, email, password } = currentElement;
+                            return (
                                 <div className="showDataStyle" key={id}>
                                     <p>{email}</p>
                                     <p>{password}</p>
@@ -57,7 +57,7 @@ const BasicForm = () => {
                                 </div>
                             )
                         })
-                        
+
                     }
                 </div>
             </div>
