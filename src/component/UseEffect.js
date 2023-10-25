5f4f704576879c05eb164a4366d6d461488eadee
kranti waghmare <krantiwaghmare21@navgurukul.org>
2023-10-25 10:47:48 +0530

    use useState hook and useEffect hook in component

--- src/component/UseEffect.js
+++ src/component/UseEffect.js
@@ -1,32 +1,32 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const UseEffect = () => {
-    const [count,setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log("I am kranti")
-        if (count>=1){
+        if (count >= 1) {
             document.title = `Chats(${count})`
 
-        }else{
+        } else {
             document.title = `Chats`
         }
-      
-    },[count ]);
 
-    useEffect(()=>{
+    }, [count]);
+
+    useEffect(() => {
         console.log("inside hook");
-      
-    },[ ]);
+
+    }, []);
 
     console.log("outside hook");
-    
-    return(
+
+    return (
         <>
             <div>
                 <h1>{count}</h1>
-                <button onClick={()=> setCount(count+1)}>click</button>
+                <button onClick={() => setCount(count + 1)}>click</button>
             </div>
         </>
     )
