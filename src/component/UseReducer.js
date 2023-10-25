40eef517184f79055dd65427399394e02f5856a6
kranti waghmare <krantiwaghmare21@navgurukul.org>
2023-10-25 10:43:16 +0530

    use useReducer hook

--- src/component/UseReducer.js
+++ src/component/UseReducer.js
@@ -1,29 +1,29 @@
 import React, { useReducer } from "react";
 
-const initialState=0;
+const initialState = 0;
 
-const reducer=(state,action) => {
+const reducer = (state, action) => {
     if (action.type === "INCREMENT") {
-        return state+1;
+        return state + 1;
     }
     if (action.type === "DECREMENT") {
-        return state-1;
+        return state - 1;
     }
     return state;
 }
 
 const UseReducer = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
-    return(
-        <>  
-            
-            <div> 
+    return (
+        <>
+
+            <div>
                 <p>{state}</p>
                 <div>
-                    <button onClick={()=> dispatch({type:"INCREMENT"})}>increment</button>
-                    <button onClick={()=> dispatch({type:"DECREMENT"})}>decrement</button>
+                    <button onClick={() => dispatch({ type: "INCREMENT" })}>increment</button>
+                    <button onClick={() => dispatch({ type: "DECREMENT" })}>decrement</button>
                 </div>
-                
+
             </div>
         </>
     )
