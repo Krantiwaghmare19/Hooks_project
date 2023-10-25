1928c22187c21fb3eaa1f0710323579b87e1a932
kranti waghmare <krantiwaghmare21@navgurukul.org>
2023-10-25 10:41:45 +0530

    Done ShortCircuitEvalution using Ternary operator

--- src/component/ShortCircuitEvaluation.js
+++ src/component/ShortCircuitEvaluation.js
@@ -1,18 +1,18 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
-const ShortCircuitEvaluation =()=>{
-    const[demo,setDemo]=useState("");
+const ShortCircuitEvaluation = () => {
+    const [demo, setDemo] = useState("");
 
-    return(
+    return (
         <>
             <div>
-                <h1>{demo || 
+                <h1>{demo ||
                     <>
-                    <h1>kranti</h1>
-                    <p>You can do anything?</p>
+                        <h1>kranti</h1>
+                        <p>You can do anything?</p>
                     </>
                 }</h1>
-                    
+
                 <h1>{demo && "priti"}</h1>
 
             </div>
