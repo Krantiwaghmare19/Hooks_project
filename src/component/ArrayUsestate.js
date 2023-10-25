f701ddedadaa95b81e2a494f89983bf45823e74a
kranti waghmare <krantiwaghmare21@navgurukul.org>
2023-10-25 10:49:56 +0530

    use ArrayuseState for  removing element

--- src/component/ArrayUsestate.js
+++ src/component/ArrayUsestate.js
@@ -1,52 +1,52 @@
 import React, { useState } from "react";
 
 const ArrayUseState = () => {
-  const bioData = [
-    {
-      id: 0,
-      name: "Kranti",
-      age: 21,
-    },
-    
-    {
-      id: 1,
-      name: "Priti",
-      age: 20,
-    },
-    
-    {
-      id: 2,
-      name: "Divya",
-      age: 22,
-    },
-  ];
+    const bioData = [
+        {
+            id: 0,
+            name: "Kranti",
+            age: 21,
+        },
 
-  const [array, setArray] = useState(bioData);
+        {
+            id: 1,
+            name: "Priti",
+            age: 20,
+        },
 
-  const clearArray = () => {
-    setArray([]);
-  };
+        {
+            id: 2,
+            name: "Divya",
+            age: 22,
+        },
+    ];
 
-  const removeElement = (id) => {
-    const newArray = array.filter((data) => data.id !== id);
-    setArray(newArray);
-  };
+    const [array, setArray] = useState(bioData);
 
-  return (
-    <>
-      {array.map((data) => {
-        return (
-          <div key={data.id} className="h1style">
-            Name: {data.name} Age: {data.age}
-            <button onClick={() => removeElement(data.id)}>Remove</button>
-          </div>
-        );
-      })}
-      <button className="btn" onClick={clearArray}>
-        Clear
-      </button>
-    </>
-  );
+    const clearArray = () => {
+        setArray([]);
+    };
+
+    const removeElement = (id) => {
+        const newArray = array.filter((data) => data.id !== id);
+        setArray(newArray);
+    };
+
+    return (
+        <>
+            {array.map((data) => {
+                return (
+                    <div key={data.id} className="h1style">
+                        Name: {data.name} Age: {data.age}
+                        <button onClick={() => removeElement(data.id)}>Remove</button>
+                    </div>
+                );
+            })}
+            <button className="btn" onClick={clearArray}>
+                Clear
+            </button>
+        </>
+    );
 };
 
 export default ArrayUseState;
